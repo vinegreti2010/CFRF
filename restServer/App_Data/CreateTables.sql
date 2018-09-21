e6a0db1183ac03b081c2bfa001d1d828e7159d27
vinegreti2010 <vinegreti2010@hotmail.com>
2018-09-21 11:52:06 -0300

    Database script added

---
+++ restServer/App_Data/CreateTables.sql
@@ -0,0 +1,85 @@
+CREATE TABLE opr_defn (
+  user_id			VARCHAR(20)		NOT NULL,
+  password_user		VARCHAR(64)		NOT NULL,
+  access			CHAR(1)			NOT NULL	DEFAULT 'N',
+  PRIMARY KEY(user_id)
+);
+
+CREATE TABLE personal_data (
+  student_id		INTEGER			NOT NULL	IDENTITY(1, 1),
+  name_display		VARCHAR(50),
+  PRIMARY KEY(student_id)
+);
+
+CREATE TABLE term_tbl (
+  strm				INTEGER			NOT NULL,
+  descr				VARCHAR(20),
+  begin_dt			DATE			NOT NULL,
+  end_dt			DATE			NOT NULL,
+  PRIMARY KEY(strm)
+);
+
+CREATE TABLE facility_tbl (
+  facility_id		INTEGER			NOT NULL	IDENTITY(1, 1),
+  descr				VARCHAR(30),
+  coordinate_north_east INTEGER,
+  coordinate_north_west INTEGER,
+  coordinate_south_east INTEGER,
+  coordinate_south_west INTEGER,
+  PRIMARY KEY(facility_id)
+);
+
+CREATE TABLE student_images (
+  student_id		INTEGER		NOT NULL,
+  student_image		IMAGE		NOT NULL,
+  embedding			TEXT,
+  PRIMARY KEY(student_id),
+  FOREIGN KEY(student_id)
+    REFERENCES personal_data(student_id)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE class_tbl (
+  class_nbr			INTEGER		NOT NULL	IDENTITY(1, 1),
+  strm				INTEGER		NOT NULL,
+  descr				VARCHAR(30),
+  facility_id		INTEGER		NOT NULL,
+  PRIMARY KEY(class_nbr, strm),
+  FOREIGN KEY(facility_id)
+    REFERENCES facility_tbl(facility_id)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(strm)
+    REFERENCES term_tbl(strm)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE stdnt_enrl (
+  student_id		INTEGER		NOT NULL,
+  class_nbr			INTEGER		NOT NULL,
+  strm				INTEGER		NOT NULL,
+  PRIMARY KEY(student_id, class_nbr, strm),
+  FOREIGN KEY(student_id)
+    REFERENCES personal_data(student_id)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(class_nbr, strm)
+    REFERENCES class_tbl(class_nbr, strm)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+CREATE TABLE class_attendence (
+  class_nbr			INTEGER		NOT NULL,
+  strm				INTEGER		NOT NULL,
+  student_id		INTEGER		NOT NULL,
+  attend_dt			DATETIME	NOT NULL,
+  presence			CHAR(1)		DEFAULT 'N',
+  PRIMARY KEY(class_nbr, strm, student_id, attend_dt),
+  FOREIGN KEY(student_id, class_nbr, strm)
+    REFERENCES stdnt_enrl(student_id, class_nbr, strm)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
--- restServer/BackEnd/cSharp/DataBaseHandler.cs
+++ restServer/BackEnd/cSharp/DataBaseHandler.cs
@@ -1,9 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
+using Informations;
+using Newtonsoft.Json;
 
 namespace cSharp {
-    public class DataBaseHandler {
+    public sealed class DataBaseHandler {
+
+        private static DataBaseHandler instance = null;
+        private static readonly object padlock = new object();
+        private SqlConnectionStringBuilder builder;
+
+        private DataBaseHandler() {
+            builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "cfrfdbserver.database.windows.net";
+            builder.UserID = "vinicius";
+            builder.Password = "Vine!@#1015Souto";
+            builder.InitialCatalog = "CFRFDataBase";
+        }
+
+        public static DataBaseHandler GetInstance {
+            get {
+                lock(padlock) {
+                    if(instance == null) {
+                        instance = new DataBaseHandler();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public List<object> ExecuteQuery(string query) {
+            try {
+                List<object> returnValues = new List<object>();
+                using(SqlConnection connection = new SqlConnection(builder.ConnectionString)) {
+                    connection.Open();
+                    using(SqlCommand command = new SqlCommand(query, connection)) {
+                        using(SqlDataReader reader = command.ExecuteReader()) {
+                            while(reader.Read()) {
+                                for(int i = 0; i < reader.VisibleFieldCount; i++) {
+                                    returnValues.Add(reader.GetString(i));
+                                }
+                            }
+                        }
+                    }
+                }
+                return returnValues;
+
+                
+            } catch {
+                ResponseInfo response = new ResponseInfo {
+                    code = "7",
+                    header = "Erro",
+                    message = "Não foi possível conectar ao banco de dados"
+                };
+                throw new Exception(JsonConvert.SerializeObject(response));
+            }
+        }
     }
 }
